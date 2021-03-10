8163e4d9f384345d1c3de8e1c8edec578334ab25
i-saint <saint.skr@gmail.com>
2021-03-10 22:32:53 +0900

    forgot to add .sln

--- src/Test/pch.h
+++ src/Test/pch.h
@@ -1,14 +1,20 @@
 #pragma once
-
 #include <cstdio>
 #include <string>
+#include <cstdarg>
 #include <vector>
 #include <map>
 #include <algorithm>
 #include <functional>
 #include <memory>
 #include <iostream>
+#include <chrono>
 
 #ifdef __cpp_lib_span
     #include <span>
+#endif
+
+#ifdef _WIN32
+    #define NOMINMAX
+    #include <windows.h>
 #endif
